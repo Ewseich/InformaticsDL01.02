8a345950be3697da1e912c0d02b76ce5f5c9523a
Ewseich <timapoton@gmail.com>
2025-01-30 21:52:16 +0300

    ready

--- src/task1445.java
+++ src/task1445.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class task1445 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt(), n = sc.nextInt(), x = sc.nextInt(), y = sc.nextInt();
+        if (x > 1) System.out.println((x-1) + " " + y);
+        if (y > 1) System.out.println(x + " " + (y-1));
+        if (x < m) System.out.println((x+1) + " " + y);
+        if (y < n) System.out.println(x + " " + (y+1));
+
+
+
+    }
 }
